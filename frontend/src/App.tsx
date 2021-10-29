856a9a22c937edc0590109fbfab96923b646afc4
Fazil Zuhaib <zuhaibfazil@gmail.com>
2021-10-28 23:55:28 -0400

    basic design unit flow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
-import './App.css';
+import { useState } from 'react';
+import { Search } from './components/search';
+import { Unit } from './components/unit';
 
 function App() {
+  // const [term, setTerm] = useState('');
   return (
     <div className="App">
+      <Unit name="goredrinker" />
     </div>
   );
 }
--- frontend/src/components/unit.tsx
+++ frontend/src/components/unit.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { PatchChange } from '../interfaces/PatchChange';
 import { Change } from './change';
+import { Summary } from './summary';
 
 interface Props {
     name: string
@@ -13,12 +14,15 @@ export const Unit: React.FC<Props> = ({ name }) => {
         console.log(name)
         const data = require('../modelData/goredrinker.json');
         setChanges(data);
-    }, [])
+    }, []);
     return (
-        <section>
-        {changes.map(({ changes, context, patch }, i) => 
-            <Change changes={changes} context={context} patch={patch} key={i} />
-        )}
-        </section>
-    )
+        <div className='page'>
+            <Summary name={name}/>
+            <section>
+            {changes.map(({ changes, context, patch }, i) => 
+                <Change changes={changes} context={context} patch={patch} key={i} />
+            )}
+            </section>
+        </div>
+    );
 }
